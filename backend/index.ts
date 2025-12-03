7b042076c46ab425ed3b57d2d1801bb1d79ab4b3
John Rocha <xjohnfitcodes@gmail.com>
2025-12-03 10:28:18 -0500

    removed not used path import

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,6 @@
 // Dependencies imports
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 
