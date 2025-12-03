e9573f83c75fe3c9e20aa0c3473e948a50eee7e8
John Rocha <xjohnfitcodes@gmail.com>
2025-12-03 08:13:25 -0500

    Fix workout API slice

--- frontend/src/slices/workoutApiSlice.ts
+++ frontend/src/slices/workoutApiSlice.ts
@@ -1,9 +1,6 @@
 import { apiSlice } from '@/slices/apiSlice';
 
-const BASE_URL =
-    import.meta.env.VITE_MODE === 'development'
-        ? 'http://localhost:5004/api'
-        : 'https://api.fitverse.codewithxjohn.com';
+const BASE_URL = '/workouts';
 
 export const workoutApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
